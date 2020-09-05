1ff6373b92c9815c6b53a13e9a0365efb94d6bdb
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2020-09-05 17:43:31 -0400

    adding components.

--- friends/src/Components/login.js
+++ friends/src/Components/login.js
@@ -18,5 +18,37 @@ class Login extends React.Component {
         });
     };
 
-    login
-}
+    login = (e) => {
+        e.preventDefault();
+        axios
+            .post("http://localhost:5000/api/login", this.state.credentials)
+            .then((res) => {
+                localStorage.setItem("token", res.data.payload);
+                this.props.history.push("/protected");
+            })
+            .catch ((err) => console.log(err));
+    };
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.login}>
+                    <input
+                        type='text'
+                        name='username'
+                        value={this.state.credentials.username}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type='text'
+                        name='password'
+                        value={this.state.credentials.password}
+                        onChange={this.handleChange}
+                    />
+                    <button>Log In</button>
+                </form>
+            </div>
+        );
+    }
+    }
+export default Login;
